6ea1d1eb42ff6e0100f66ba2011ffbd2b6bbe5cf
Amanuel Adamu <amanueladamu18@gmail.com>
2022-01-26 08:34:07 +0000

    added solution for task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,19 @@ from uuid import uuid4
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
 
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.update_at = datetime.today()
 
-        
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "update_at":
+                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[k] = v
+
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
         
@@ -22,4 +28,4 @@ class BaseModel:
         cdict["created_at"] = self.created_at.isoformat()
         cdict["update_at"] =  self.update_at.isoformat()
         cdict["__class__"] = self.__class__.__name__
-        return cdict    
+        return cdict
