128c127a57db409acac3b01e9df90f937e2eafeb
Amanuel Adamu <amanueladamu18@gmail.com>
2022-01-28 06:52:28 +0000

    redoing task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
+"""Defining base_model"""
 
+import models
 from datetime import datetime
 from uuid import uuid4
 
 class BaseModel:
+    """Defining a BaseModel class that defines all common attributes/methods for other classes"""
 
-    def __init__(self, *args, **kwargs):
-
+    def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.update_at = datetime.today()
-
-        if kwargs is not None:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "update_at":
-                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[k] = v
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-        
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
     def save(self):
-        self.update_at = datetime.today()
+        self.updated_at = datetime.now()
+
     def to_dict(self):
-        cdict = self.__dict__.copy()
-        cdict["created_at"] = self.created_at.isoformat()
-        cdict["update_at"] =  self.update_at.isoformat()
-        cdict["__class__"] = self.__class__.__name__
-        return cdict
+        instance_dict = dict(self.__dict__)
+        instance_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        instance_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        instance_dict['__class__'] = self.__class__.__name__
+        return instance_dict
